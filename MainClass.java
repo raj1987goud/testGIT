67d54391d86874b79d242c567504c798484e1cf6
Balraj <balraj@dprsystems.com>
2016-11-18 19:40:02 +0530

    new file added

---
+++ MainClass.java
@@ -0,0 +1,31 @@
+package com.test;
+
+public class MainClass {
+
+	private String name;
+	private String id;
+	private String nation;
+	private String state;
+	public MainClass() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public MainClass(String name, String id, String nation, String state) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.nation = nation;
+		this.state = state;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("main class method ex");
+	}
+	
+	
+	private void m1() {
+		System.out.println("m1 method");
+	}
+}
