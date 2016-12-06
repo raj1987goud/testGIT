5bcd0d307a918c9153b60ba73afcd4388bdfc592
Balraj <balraj@dprsystems.com>
2016-12-06 21:33:38 +0530

    changes made for main class version 1

--- MainClass.java
+++ MainClass.java
@@ -6,15 +6,18 @@ public class MainClass {
 	private String id;
 	private String nation;
 	private String state;
+	private String stateNewAppend;
+	
 	public MainClass() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public MainClass(String name, String id, String nation, String state) {
+	public MainClass(String name, String id, String nation, String state,String stateNewAppend) {
 		super();
 		this.name = name;
 		this.name = name;
 		this.nccc = name;
+		this.stateNewAppend = stateNewAppend;
 		this.id = id;
 		this.nation = nation;
 		this.state = state;
